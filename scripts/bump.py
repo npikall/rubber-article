45a3f2d65a5da13a549d9cd3d70656141bf3bf50
Nikolas Pikall <nikolas.pikall@gmail.com>
2026-01-18 17:06:05 +0100

    chore: update bump script

--- scripts/bump.py
+++ scripts/bump.py
@@ -1,13 +1,24 @@
+# /// script
+# requires-python = ">=3.14"
+# dependencies = []
+# ///
+
 """Bump the version of the Typst package in various files."""  # noqa: INP001
 
 import logging
 import re
 import sys
+from enum import IntEnum, auto
 from pathlib import Path
 
 FILES_TO_UPDATE = ["typst.toml", "README.md", "template/main.typ", "docs/docs.typ"]
 logger = logging.getLogger(__name__)
-logger.basicConfig(level=logging.INFO, format="%(message)s")
+logging.basicConfig(level=logging.INFO, format="%(message)s")
+
+
+class ExitCodes(IntEnum):
+    SUCCESS = 0
+    ERROR = 1
 
 
 def update_version(old: str, new: str, file: Path) -> None:
@@ -24,12 +35,12 @@ def update_version(old: str, new: str, file: Path) -> None:
     logger.info(msg)
 
 
-def main() -> None:
+def main() -> int:
     """Handle command line arguments and update versions."""
     max_args = 3
     if len(sys.argv) != max_args:
         logger.error("Usage: python bump.py <old_version> <new_version>")
-        sys.exit(1)
+        return ExitCodes.ERROR
 
     root = Path.cwd()
     old_version = sys.argv[1]
@@ -38,6 +49,8 @@ def main() -> None:
     for file in FILES_TO_UPDATE:
         update_version(old_version, new_version, root / file)
 
+    return ExitCodes.SUCCESS
+
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
